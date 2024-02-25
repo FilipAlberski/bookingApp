c13ce338fca5acae2368bef8b0b7a02585a56c13
Filip Alberski <alberskif@gmail.com>
2024-02-25 18:20:47 +0100

    Update RegisterFormData type definition

--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
 
-type RegisterFormData = {
+export type RegisterFormData = {
   firstName: string;
   lastName: string;
   email: string;
