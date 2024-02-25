c4a86d0cbb5a2cac7bf081c9e8d4ce6a5d2f7fcc
Filip Alberski <alberskif@gmail.com>
2024-02-25 11:10:48 +0100

    create login func

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import usersRoute from './routes/usersRoute';
+import authRoute from './routes/auth';
 
 import mongoose from 'mongoose';
 
@@ -13,7 +14,8 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/apu/users', usersRoute);
+app.use('/api/auth', authRoute);
+app.use('/api/users', usersRoute);
 
 app.listen(7001, () => {
   console.log(`Server is running on port ${7001}`);
