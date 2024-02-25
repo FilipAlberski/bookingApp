39dd851b69157835b7bfe697b0c85b30e96f0a1e
Filip Alberski <alberskif@gmail.com>
2024-02-25 10:51:01 +0100

    Add bcrypt for password hashing and JWT secret key

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
 import 'dotenv/config';
+import usersRoute from './routes/usersRoute';
 
 import mongoose from 'mongoose';
 
@@ -12,9 +13,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/api/test', async (req: Request, res: Response) => {
-  res.json({ message: 'Hello World! /api/test' });
-});
+app.use('/apu/users', usersRoute);
 
 app.listen(7001, () => {
   console.log(`Server is running on port ${7001}`);
