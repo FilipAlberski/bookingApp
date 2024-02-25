49a32c12914b4942bc39c695141c81b567e2ee66
Filip Alberski <alberskif@gmail.com>
2024-02-25 18:08:58 +0100

    adding register form with validation

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Layout from './layouts/Layout';
+import Register from './pages/Register';
 function App() {
   return (
     <Router>
@@ -18,6 +19,22 @@ function App() {
           element={
             <Layout>
               <span className="text-4xl">search</span>
+            </Layout>
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <Layout>
+              <span className="text-4xl">login</span>
+            </Layout>
+          }
+        />
+        <Route
+          path="/register"
+          element={
+            <Layout>
+              <Register />
             </Layout>
           }
         />
