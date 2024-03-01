59c391a9c58e8e9c5c3649695850020923616768
Filip Alberski <alberskif@gmail.com>
2024-03-01 20:06:43 +0100

    Add conditional rendering for user authentication in Header component

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useAppContext } from '../contexts/AppContext';
 
 const Header = () => {
+  const { isLoggedIn } = useAppContext();
   return (
     <div className="bg-blue-800 py-6 ">
       <div className="container mx-auto flex justify-between">
@@ -9,9 +11,20 @@ const Header = () => {
           <Link to="/">Booking</Link>
         </span>
         <span className="flex space-x-2">
-          <Link to="/sign-in" className="flex items-center bg-white text-blue-600 px-3 font-bold hover:bg-gray-100">
-            Sign in
-          </Link>
+          {isLoggedIn ? (
+            <>
+              <Link to="/my-bookings">My bookings</Link>
+              <Link to="/my-hotels">My hotels</Link>
+              <button>Sign out</button>
+            </>
+          ) : (
+            <Link
+              to="/sign-in"
+              className="flex items-center bg-white text-blue-600 px-3 font-bold hover:bg-gray-100"
+            >
+              Sign in
+            </Link>
+          )}
         </span>
       </div>
     </div>
